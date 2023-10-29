cc39aaddac73fc0044fcff1300e52623db9ed379
Tanveer <tnv.641@gmail.com>
2023-10-29 23:36:25 +0530

    Addd title in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
 import profilePic from "../../public/images//profile/developer-pic-1.png";
+import AnimatedText from "@/components/AnimatedText";
 
 export default function Home() {
   return (
@@ -18,7 +19,7 @@ export default function Home() {
               <Image src={profilePic} alt="CodeBucks" className='w-full h-auto' />
             </div>
             <div className="w-1/2">
-              <h1>Turning Vision Into Reality With Code And Design.</h1>
+              <AnimatedText text="Turning Vision Into Reality With Code And Design." className="!text-6xl" />
               <p>
               As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
 Explore my latest projects and articles, showcasing my expertise in React.js and web development.
