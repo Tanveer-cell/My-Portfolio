f9fd0b3ec5602393ef765ac787d5acb25f5a5907
Tanveer <tnv.641@gmail.com>
2023-11-04 18:51:27 +0530

    Completed Home page

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -30,24 +30,22 @@ const singleWord = {
 
 const AnimatedText = ({text, className=""}) => {
   return (
-    <div className='w-full mx-auto py-2 flex items-center justify-center text-center
-    overflow-hidden
-    '>
-        <motion.h1 className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className}`}
-        variants={quote}
-        initial="initial"
-        animate="animate"
-        >
+    <div className='w-full mx-auto py-2 flex items-center justify-center text-centeroverflow-hidden'>
+      <motion.h1 className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className}`}
+      variants={quote}
+      initial="initial"
+      animate="animate"
+      >
         {
-            text.split(" ").map((word, index) =>
-            <motion.span key={word+'-'+index} className='inline-block'
-            variants={singleWord}
-            >
-                {word}&nbsp;
-            </motion.span>
+          text.split(" ").map((word, index) =>
+          <motion.span key={word+'-'+index} className='inline-block'
+          variants={singleWord}
+          >
+              {word}&nbsp;
+          </motion.span>
         )
         }    
-        </motion.h1>
+      </motion.h1>
     </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,10 +11,9 @@ const CustomLink = ({href, title, className=""}) => {
     <Link href={href} className={`${className} relative group`}>
       {title}
 
-      <span className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5
-      group-hover:w-full transition-[width] ease duration-300
-      ${router.asPath === href ? 'w-full' : 'w-0'}
-      `}>&nbsp;</span>
+      <span className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}>
+        &nbsp;
+      </span>
     </Link>
   )
 }
