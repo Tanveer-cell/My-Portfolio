9a09d5f88c9fe32375c04186caf9628d8d154aed
Tanveer <tnv.641@gmail.com>
2023-11-09 16:24:32 +0530

    Completed Projects page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ const Footer = () => {
         <Layout className='py-8 flex items-center justify-between'>
             <span>{new Date().getFullYear()} &copy; All rights reserved.</span>
             <div className='flex-items-center'>
-              Build with <span className='text-primary text-2xl px-1'>&#9825;</span>
-              by &nbsp; <Link href='https://www.instagram.com/ig_veer6/' className='underline underline-offset-2' target={"_blank"}>Tanveer</Link>
+              Build with<span className='text-primary text-2xl px-1'>&#9825;</span>
+              by&nbsp;<Link href='https://www.instagram.com/ig_veer6/' className='underline underline-offset-2' target={"_blank"}>Tanveer</Link>
             </div>
             <Link href="https://www.instagram.com/ig_veer6/" target={"_blank"} className='underline underline-offset-2'>Say Hello</Link>
         </Layout>
