ff171e1e363c89f4cde6bf5bfacff4802703af3f
Tanveer <tnv.641@gmail.com>
2023-11-13 12:33:26 +0530

    Made Responsive

--- src/pages/about.js
+++ src/pages/about.js
@@ -41,9 +41,9 @@ const about = () => {
         </Head>
         <main className='flex w-full flex-col items-center justify-center dark:text-light'>
             <Layout className='pt-16'>
-                <AnimatedText text="Passion Fuels Purpose!" className='mb-16'/>
-                <div className='grid w-full grid-cols-8 gap-16'>
-                    <div className='col-span-3 flex flex-col items-start justify-start'>
+                <AnimatedText text="Passion Fuels Purpose!" className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8'/>
+                <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
+                    <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8'>
                         <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>About</h2>
                         <p className='font-medium'>
                             Hi, I&apos;m Tanveer, a 3<sup>rd</sup> year Biotechnology student at Haldia Institute of Technology, Haldia, West Bengal, India. I have keen interest in Science and Technology and always enthusiastic about trying and learning new things to expand my knowledge and understant the world around me in a better way.
@@ -57,7 +57,8 @@ const about = () => {
                             Currently I&apos;m working on developing my skills in field of Mathematics, Computer Science and Biotechnology so I can apply those concepts to real world problems and to solve them in most efiicient manner and provide something valueable to the people.
                         </p>
                     </div>
-                    <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light'>
+
+                    <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8'>
                         <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light' />
                         <Image src={profilePic} alt="Tanveer" className='w-full h-auto rounded-2xl'
                         priority
@@ -65,27 +66,31 @@ const about = () => {
                         />
                     </div>
 
-                    <div className='col-span-2 flex flex-col items-end justify-between'>
+                    <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3'>
 
-                        <div className='flex flex-col items-end justify-center'>
-                            <span className='inline-block text-7xl font-bold'>
+                        <div className='flex flex-col items-end justify-center xl:items-center'>
+                            <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                                 <AnimatedNumbers value={10} />+
                             </span>
-                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>Template</h2>
+                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base
+                            '>
+                                Template</h2>
                         </div>
 
-                        <div className='flex flex-col items-end justify-center'>
-                            <span className='inline-block text-7xl font-bold'>
+                        <div className='flex flex-col items-end justify-center xl:items-center'>
+                            <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                                 <AnimatedNumbers value={3} />+
                             </span>
-                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>Projects Completed</h2>
+                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base
+                            '>Projects Completed</h2>
                         </div>
 
-                        <div className='flex flex-col items-end justify-center'>
-                            <span className='inline-block text-7xl font-bold'>
+                        <div className='flex flex-col items-end justify-center xl:items-center'>
+                            <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                                 <AnimatedNumbers value={1} />+
                             </span>
-                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>Years of experience</h2>
+                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base
+                            '>Years of experience</h2>
                         </div>
 
                     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,30 +17,32 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </Head>
       <main className='flex items-center text-dark w-full min-h-screen dark:text-light'>
-        <Layout className="pt-0">
-          <div className="flex items-center justify-between w-full">
-            <div className="w-1/2">
-              <Image src={profilePic} alt="Humanoid" className='w-9/12 h-auto pl-11'
+        <Layout className="pt-0 md:pt-16 sm:pt-8">
+          <div className="flex items-center justify-between w-full lg:flex-col">
+            <div className="w-1/2 md:w-9/12">
+              <Image src={profilePic} alt="Humanoid" className='w-9/12 h-auto pl-11 lg:hidden md:inline-block md:w-full'
               priority
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
               />
             </div>
-            <div className="w-1/2 flex flex-col items-center self-center">
-              <AnimatedText text="Combinig Life and Technology to Turn Vision Into Reality." className="!text-6xl !text-left" />
-              <p className="my-4 text-base font-medium">
+            <div className="w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center">
+              <AnimatedText text="Combinig Life and Technology to Turn Vision Into Reality." className="!text-6xl !text-left 
+              xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl
+              "/>
+              <p className="my-4 text-base font-medium md:text-sm sm:text-xs">
               &quot; We didn&apos;t stay in the caves. We haven&apos;t stayed on the planet. With biotechnology, gene sequencing, we are not going to even stay within the limitations of biology. &quot;
               </p>
-              <div className="flex items-center self-start mt-2">
+              <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link href="/dummy.pdf" target={"_blank"}
                 className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
                 border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark
-                hover:dark:text-light hover:dark:border-light"
+                hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
                 download={true}
                 >Resume
                 <LinkArrow className={"w-6 ml-1"}/>
                 </Link>
                 <Link href="mailto:tnv.641@gmail.com" target={"_blank"}
-                className="ml-4 text-lg font-medium capitalize text-dark underline dark:text-light"
+                className="ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base"
                 >Contact</Link>
               </div>
             </div>
@@ -48,7 +50,7 @@ export default function Home() {
         </Layout>
 
         <HireMe/>
-        <div className="absolute right-8 bottom-8 inline-block w-24">
+        <div className="absolute right-8 bottom-8 inline-block w-24 md:hidden">
           <Image src={lightBulb} alt="idea" className="w-full h-auto" />
         </div>
       </main>
