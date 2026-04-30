20bf35968e05051315b9df49e1398cf4e2836333
Tanveer <tnv.641@gmail.com>
2026-04-30 07:48:29 +0530

    Fix ESLint compatibility and dev origin config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withMDX = require('@next/mdx')();
 module.exports = withMDX({
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  allowedDevOrigins: ['10.20.74.108'],
   // Optionally, add any other Next.js config below
   turbopack: {},
 });
